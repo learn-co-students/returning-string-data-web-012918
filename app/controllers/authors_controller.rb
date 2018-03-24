96b83d9e9631e2cc411144f997220bff82ac9f8f
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-24 13:39:16 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,10 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+  
 end
